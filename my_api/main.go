e0fc02a4c1b0fcfd9c64eb2ad6bbea75060a5705
KunoYasuyuki <kunoyasuyuki@yahoo.co.jp>
2023-03-25 15:56:26 +0900

    コメントアウトの追記

--- my_api/main.go
+++ my_api/main.go
@@ -7,10 +7,16 @@ import (
 )
 
 func main() {
+	// helloHandlerという名前で、ハンドラを定義
 	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		// ハンドラの処理内容:
+		// 何がきても、"Hello, World!"の文字列を返す
 		io.WriteString(w, "Hello, world!\n")
 	}
+	// 定義した helloHandler を使うように登録
 	http.HandleFunc("/", helloHandler)
+	// サーバー起動時のログを出力
 	log.Println("server start at port 8080")
+	// ListenAndServe関数にて、サーバーを起動
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
